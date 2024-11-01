57670c25ba52d17a95082ddd1c7820d03e69110d
Elvis Lopes <evs.lopes@gmail.com>
2024-11-01 19:40:25 -0300

    Commit inicial

---
+++ src/main/java/com/desafioserasaexp/api/DesafioSerasaExpApplication.java
@@ -0,0 +1,13 @@
+package com.desafioserasaexp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSerasaExpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioSerasaExpApplication.class, args);
+    }
+
+}
